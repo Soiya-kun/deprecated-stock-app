1a567ee78e4bebf649f4e485e4c0838db8002463
Soiya-kun <s-system@seiwa-ic.co.jp>
2023-03-24 16:34:41 +0900

    fix css

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -58,131 +58,134 @@ export function TopPage() {
 
   return (
     <div className="flex">
-      <div className="w-1/3">
-        <Chart
-          chartType="ComboChart"
-          width="100%"
-          height="400px"
-          data={(
-            [
+      <div className="w-1/2">test</div>
+      <div className="w-1/2">
+        <div>
+          <Chart
+            chartType="ComboChart"
+            width="100%"
+            height="400px"
+            data={(
               [
-                "Date",
-                "High",
-                "Open",
-                "Close",
-                "Low",
-                "ma5",
-                "ma20",
-                "ma60",
-                "volume",
-              ],
-            ] as GraphValue[]
-          ).concat(
-            svDay.slice(
-              dateState.dayBeforeCount,
-              dateState.dayCount + dateState.dayBeforeCount,
-            ),
-          )}
-          options={{
-            seriesType: "candlesticks",
-            series: {
-              1: {
-                type: "line",
+                [
+                  "Date",
+                  "High",
+                  "Open",
+                  "Close",
+                  "Low",
+                  "ma5",
+                  "ma20",
+                  "ma60",
+                  "volume",
+                ],
+              ] as GraphValue[]
+            ).concat(
+              svDay.slice(
+                dateState.dayBeforeCount,
+                dateState.dayCount + dateState.dayBeforeCount,
+              ),
+            )}
+            options={{
+              seriesType: "candlesticks",
+              series: {
+                1: {
+                  type: "line",
+                },
+                2: {
+                  type: "line",
+                },
+                3: {
+                  type: "line",
+                },
+                4: {
+                  type: "bars",
+                  targetAxisIndex: 1,
+                },
               },
-              2: {
-                type: "line",
+              vAxes: {
+                0: { title: "円" },
+                1: {
+                  title: "株",
+                  minValue: 0,
+                  maxValue:
+                    Number(
+                      svDay
+                        .slice(
+                          dateState.dayBeforeCount,
+                          dateState.dayCount + dateState.dayBeforeCount,
+                        )
+                        .sort((a, b) => Number(b[8]) - Number(a[8]))[0][8],
+                    ) * 4,
+                },
               },
-              3: {
-                type: "line",
+              chartArea: {
+                left: "15%",
+                width: "70%",
               },
-              4: {
-                type: "bars",
-                targetAxisIndex: 1,
-              },
-            },
-            vAxes: {
-              0: { title: "円" },
-              1: {
-                title: "株",
-                minValue: 0,
-                maxValue:
-                  Number(
-                    svDay
-                      .slice(
-                        dateState.dayBeforeCount,
-                        dateState.dayCount + dateState.dayBeforeCount,
-                      )
-                      .sort((a, b) => Number(b[8]) - Number(a[8]))[0][8],
-                  ) * 4,
-              },
-            },
-            chartArea: {
-              left: "15%",
-              width: "70%",
-            },
-          }}
-        />
-      </div>
-      <div className="w-1/3">
-        <Chart
-          chartType="ComboChart"
-          width="100%"
-          height="400px"
-          data={(
-            [
+            }}
+          />
+        </div>
+        <div>
+          <Chart
+            chartType="ComboChart"
+            width="100%"
+            height="400px"
+            data={(
               [
-                "Date",
-                "High",
-                "Open",
-                "Close",
-                "Low",
-                "volume",
-                "ma13",
-                "ma26",
-              ],
-            ] as GraphValue[]
-          ).concat(
-            svWeek.slice(
-              dateState.weekBeforeCount,
-              dateState.weekCount + dateState.weekBeforeCount,
-            ),
-          )}
-          options={{
-            seriesType: "candlesticks",
-            series: {
-              1: {
-                type: "bars",
-                targetAxisIndex: 1,
+                [
+                  "Date",
+                  "High",
+                  "Open",
+                  "Close",
+                  "Low",
+                  "volume",
+                  "ma13",
+                  "ma26",
+                ],
+              ] as GraphValue[]
+            ).concat(
+              svWeek.slice(
+                dateState.weekBeforeCount,
+                dateState.weekCount + dateState.weekBeforeCount,
+              ),
+            )}
+            options={{
+              seriesType: "candlesticks",
+              series: {
+                1: {
+                  type: "bars",
+                  targetAxisIndex: 1,
+                },
+                2: {
+                  type: "line",
+                },
+                3: {
+                  type: "line",
+                },
               },
-              2: {
-                type: "line",
+              vAxes: {
+                0: { title: "円" },
+                1: {
+                  title: "株",
+                  minValue: 0,
+                  maxValue:
+                    Number(
+                      svWeek
+                        .slice(
+                          dateState.weekBeforeCount,
+                          dateState.weekCount + dateState.weekBeforeCount,
+                        )
+                        .sort((a, b) => Number(b[5]) - Number(a[5]))[0][5],
+                    ) * 4,
+                },
               },
-              3: {
-                type: "line",
+              chartArea: {
+                left: "15%",
+                width: "70%",
               },
-            },
-            vAxes: {
-              0: { title: "円" },
-              1: {
-                title: "株",
-                minValue: 0,
-                maxValue:
-                  Number(
-                    svWeek
-                      .slice(
-                        dateState.weekBeforeCount,
-                        dateState.weekCount + dateState.weekBeforeCount,
-                      )
-                      .sort((a, b) => Number(b[5]) - Number(a[5]))[0][5],
-                  ) * 4,
-              },
-            },
-            chartArea: {
-              left: "15%",
-              width: "70%",
-            },
-          }}
-        />
+            }}
+          />
+        </div>
       </div>
     </div>
   );
